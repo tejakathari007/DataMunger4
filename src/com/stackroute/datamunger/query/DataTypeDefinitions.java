a2e056883d04573355100987d287d36963ca0beb
Farook.a <a.farook@stackroute.in>
2017-08-30 18:08:18 +0530

    step 4 Boilerplate

---
+++ src/com/stackroute/datamunger/query/DataTypeDefinitions.java
@@ -0,0 +1,13 @@
+package com.stackroute.datamunger.query;
+
+//this class contains the data type definitions
+public class DataTypeDefinitions {
+
+	/*
+	 * this class should contain a member variable which is a String array, to hold
+	 * the data type for all columns for all data types
+	 */	
+	public String[] getDataTypes() {
+		return null;
+	}
+}
---
+++ src/com/stackroute/datamunger/query/Header.java
@@ -0,0 +1,13 @@
+package com.stackroute.datamunger.query;
+
+//header class
+public class Header {
+
+	/*
+	 * this class should contain a member variable which is a String array, to hold
+	 * the headers.
+	 */
+	public String[] getHeaders() {
+		return null;
+	}
+}
